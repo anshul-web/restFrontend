6ca879e5b52a7b331e115049838b3257e60466fb
anshul-web <65392052+anshul-web@users.noreply.github.com>
2020-10-12 23:38:06 +0530

    restFrontend

---
+++ build/precache-manifest.855c8fa6a3054749a440b1223f8180ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb150cc6bdf46c8ed74d43944dc3f92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a9999b304ceda914fad",
+    "url": "/static/css/main.3dafb1ca.chunk.css"
+  },
+  {
+    "revision": "f2b7d64320fb54d808da",
+    "url": "/static/js/2.af7ae5a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af7ae5a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9999b304ceda914fad",
+    "url": "/static/js/main.b687cb0c.chunk.js"
+  },
+  {
+    "revision": "0980db0307ffad65803b",
+    "url": "/static/js/runtime-main.df5858f9.js"
+  }
+]);
